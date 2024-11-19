1016ecf5c40165e84e612cf7d95b55bb14c6f3ff
Vbelmar <vicentebelenguer05@gmail.com>
2024-11-19 19:27:10 +0100

    Añadida implementación de la clase BSNode

---
+++ BSNode.h
@@ -0,0 +1,23 @@
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+template <typename T> 
+class BSNode {
+    public:
+    	T elem;
+	BSNode<T>* left;
+	BSNode<T>* right;
+
+	BSNode(T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr) : elem(elem), left(left), right(right) {}
+
+	friend std::ostream& operator<<(std::ostream &out, const BSNode<T> &bsn) {
+		out << "Elemento" << bsn.elem << std::endl;
+		return out;
+	}
+
+    
+};
+
+#endif
