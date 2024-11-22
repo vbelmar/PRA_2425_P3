4312cb3fa1a203e4cf147087e2e9c98f5130c5a3
Vbelmar <vicentebelenguer05@gmail.com>
2024-11-23 00:01:27 +0100

    Añadido Makefile y código de test de la clase BSTree

--- BSTree.h
+++ BSTree.h
@@ -8,17 +8,17 @@
 template <typename T> 
 class BSTree {
     private:
-	    int nelem;
-	    BSNode<T>* root;
+	    int nelem = 0;
+	    BSNode<T>* root = nullptr;
 
 
 		BSNode<T>* search(BSNode<T>* n, T e) const {
-			if(n == NULL){
+			if(n == nullptr){
 				throw std::runtime_error("Elemento no encontrado");
-			}else if(n.elem < e){
-				return search(n.right, e);
-			}else if(n.elem > e){
-				return search(n.left, e);
+			}else if(n->elem < e){
+				return search(n->right, e);
+			}else if(n->elem > e){
+				return search(n->left, e);
 			}else{
 				return n;
 			}
@@ -26,61 +26,64 @@ class BSTree {
 
 	    
 		BSNode<T>* insert(BSNode<T>* n, T e){
-	    	if(n == NULL){
+	    	if(n == nullptr){
+				nelem++;
 				return new BSNode(e);
-			}else if(n.elem == e){
+			}else if(n->elem == e){
 				throw std::runtime_error("Elemento duplicado");
-			}else if(n.elem < e){
-				n.right = insert(n.right, e);
+			}else if(n->elem < e){
+				n->right = insert(n->right, e);
 			}else{
-				n.left = insert(n.left, e);
+				n->left = insert(n->left, e);
 			}
 			return n;
 	    }
 	    
 	    void print_inorder(std::ostream &out, BSNode<T>* n) const{
-	    	if(n != NULL){
+	    	if(n != nullptr){
 				print_inorder(out, n->left);
 
-				out << n->data << " ";
+				out << n->elem << " ";
 
 				print_inorder(out, n->right);
 			}
 	    }
 
  		BSNode<T>* remove(BSNode<T>* n, T e){
-	    	if(n == NULL){
+	    	if(n == nullptr){
 				throw std::runtime_error("Elemento no encontrado");
-			}else if(n.elem < e){
-				n.right = remove(n.right, e);
-			}else if(n.elem > e){
-				n.left = remove(n.left, e);
+			}else if(n->elem < e){
+				n->right = remove(n->right, e);
+			}else if(n->elem > e){
+				n->left = remove(n->left, e);
 			}else{
-				if(n.left != NULL && n.right != NULL){
-					n.elem = max(n.left);
-					n.left = remove_max(n.left);
+				if(n->left != nullptr && n->right != nullptr){
+					nelem--;
+					n->elem = max(n->left);
+					n->left = remove_max(n->left);
 				}else{
-					n = (n.left != NULL)? n.left: n.right;
+					nelem--;
+					n = (n->left != nullptr)? n->left: n->right;
 				}
 			}
 			return n;
 	    }
 
 	    T max(BSNode<T>* n) const{
-	    	if(n == NULL){
+	    	if(n == nullptr){
 				throw std::runtime_error("Elemento no encontrado");
-			}else if(n.right != NULL){
-				return max(n.right);
+			}else if(n->right != NULL){
+				return max(n->right);
 			}else{
-				return n.elem;
+				return n->elem;
 			}
 	    }
 
 	    BSNode<T>* remove_max(BSNode<T>* n){
-	    	if(n.right == NULL){
-				return n.left;
+	    	if(n->right == nullptr){
+				return n->left;
 			}else{
-				n.right = remove_max(n.right);
+				n->right = remove_max(n->right);
 				return n;
 			}
 	    }
@@ -102,7 +105,7 @@ class BSTree {
 	    }
 
 		T search(T e) const {
-	    	return search(root, e).nelem;
+	    	return search(root, e)->elem;
 	    }
 
 	    T operator[](T e) const {
@@ -114,7 +117,9 @@ class BSTree {
 	    }
 
 	    friend std::ostream& operator<<(std::ostream &out, const BSTree<T> &bst){
-	   		return print_inorder(); 
+	   		bst.print_inorder(out, bst.root);
+		       	return out;
+			
 	    }
 	    
 		void remove(T e){
