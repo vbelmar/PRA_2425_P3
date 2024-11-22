ac3a41faa96132acf3d34321c0dcc74e656b9740
Vbelmar <vicentebelenguer05@gmail.com>
2024-11-23 00:18:49 +0100

    Añadida modificación del archivo TableEntry.h

--- TableEntry.h
+++ TableEntry.h
@@ -29,6 +29,14 @@ class TableEntry {
 		out << "{" << te.key << " -> " << te.value << "}";
         	return out;
 	}
+    	friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+    	    return te1.key < te2.key; 
+	}
+
+    	friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        	return te1.key > te2.key;
+    	}
+
     
 };
 
